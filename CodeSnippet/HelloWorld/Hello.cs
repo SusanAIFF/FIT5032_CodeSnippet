317e659c4782fee068991142dd8548f94c977ba7
Administrator <susan_aiff@163.com>
2023-06-28 17:38:53 +0800

    添加项目文件。

---
+++ CodeSnippet/HelloWorld/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSnippet.HelloWorld
+{
+    public class Hello
+    {
+        public string GetHello()
+        {
+            return "Hello World!";
+        }
+        
+    }
+
+}
---
+++ CodeSnippet/Models/Exercise/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace CodeSnippet.Models.Exercise
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public String PhoneNumber { get; set; }
+
+        public Student(String name, String phoneNumber) { 
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public class ExampleDictionary
+        {
+            public void Example()
+            { 
+                Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+                Student s1 = new Student("Ben", "123456789");
+                Student s2 = new Student("Ben2", "123456789");
+
+                studentDictionary.Add(1, s1);
+                studentDictionary.Add(2, s2);
+
+                Student result = new Student("", "");
+
+                studentDictionary.TryGetValue(1, out result);
+                public ExampleDictionary ()
+                {
+
+                }
+            }
+
+        }
+    }
+}
